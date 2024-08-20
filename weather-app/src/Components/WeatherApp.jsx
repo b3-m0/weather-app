8cc33ba27cbbff24d2a5f1f4e3720feeef695bb8
b3-m0 <bemiyamoto24@gmail.com>
2024-08-20 14:23:12 -0700

    commit one

---
+++ weather-app/src/Components/WeatherApp.jsx
@@ -0,0 +1,74 @@
+import sunny from "../assets/images/sunny.png"
+import cloudy from "../assets/images/cloudy.png"
+import rainy from "../assets/images/rainy.png"
+import snowy from "../assets/images/snowy.png"
+import { useState } from "react"
+
+const WeatherApp = () => {
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+    const api_key = '31e8758505e04a4050adf764ac2c47ec'
+
+    const handleInputChange = (e) => {
+        setLocation(e.target.value)
+    }
+
+    const search = async () => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Imperial&appid=${api_key}`
+        const res = await fetch(url)
+        const searchData = await res.json()
+        console.log(searchData)
+        setData(searchData)
+        setLocation('')
+    }
+
+    const handleKeyDown = (e) => {
+        if(e.key === "Enter") {
+            search()
+        }
+    }
+
+  return (
+    <div className="container">
+        <div className="weather-app">
+            <div className="search">
+                <div className="search-top">
+                    <i className="fa-solid fa-location-dot"></i>
+                    <div className="location">{data.name}</div>
+                </div>
+                <div className="search-bar">
+                    <input 
+                    type="text" 
+                    placeholder="Enter Location" 
+                    value={location} 
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    />
+                    <i className="fa-solid fa-magnifying-glass"onClick={search}></i>
+                </div>
+            </div>
+            <div className="weather">
+                <img src={sunny} width={250} height={250} alt="sunny" />
+                <div className="weather-type">Clear</div>
+                <div className="temp">{data.main ? `(data.main.temp)°` : null}</div>
+            </div>
+            <div className="weather-date">
+                <p>Thur, 15 Aug</p>
+            </div>
+            <div className="weather-data">
+                <div className="humidity">
+                    <div className="data-name">Humidity</div>
+                    <i className="fa-solid fa-droplet"></i>
+                    <div className="data">35%</div>
+                </div>
+                <div className="wind">
+                    <div className="data-name">Wind</div>
+                    <i className="fa-solid fa-wind"></i>
+                    <div className="data">3 km/h</div></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WeatherApp
