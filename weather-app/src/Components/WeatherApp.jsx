e32f4cce29c1c8c40f68bf42889ec525060e42c1
b3-m0 <bemiyamoto24@gmail.com>
2024-08-20 19:14:08 -0700

    added weather type smoke, finished base app

--- weather-app/src/Components/WeatherApp.jsx
+++ weather-app/src/Components/WeatherApp.jsx
@@ -1,25 +1,51 @@
 import sunny from "../assets/images/sunny.png"
+import smoky from "../assets/images/smoky.png"
 import cloudy from "../assets/images/cloudy.png"
 import rainy from "../assets/images/rainy.png"
 import snowy from "../assets/images/snowy.png"
-import { useState } from "react"
+import loadingGif from "../assets/images/loading.gif"
+import { useState, useEffect } from 'react'
+
 
 const WeatherApp = () => {
     const [data, setData] = useState({})
     const [location, setLocation] = useState('')
+    const [loading, setLoading] = useState(false)
     const api_key = '31e8758505e04a4050adf764ac2c47ec'
+
+    useEffect(() => {
+        const fetchDefaultWeather = async () => {
+            setLoading(true)
+            const defaultLocation = "New York"
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Imperial&appid=${api_key}`
+            const res = await fetch(url)
+            const defaultData = await res.json()
+            setData(defaultData)
+            setLoading(false)
+        }
+
+        fetchDefaultWeather()
+    }, [])
 
     const handleInputChange = (e) => {
         setLocation(e.target.value)
     }
 
     const search = async () => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Imperial&appid=${api_key}`
-        const res = await fetch(url)
-        const searchData = await res.json()
-        console.log(searchData)
-        setData(searchData)
-        setLocation('')
+        if(location.trim() !== "") {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Imperial&appid=${api_key}`
+            const res = await fetch(url)
+            const searchData = await res.json()
+            if(searchData.cod !== 200) {
+                setData({notFound : true})
+            } else {
+                console.log(searchData)
+                setData(searchData)
+                setLocation('')
+            }
+            setLoading(false)
+            
+        }
     }
 
     const handleKeyDown = (e) => {
@@ -28,9 +54,65 @@ const WeatherApp = () => {
         }
     }
 
+    const weatherImages = {
+        Clear: sunny,
+        Smoke: smoky,
+        Clouds: cloudy,
+        Rain: rainy,
+        Snow: snowy,
+        Haze: cloudy,
+        Mist: cloudy
+    }
+
+    const weatherImage = data.weather ? weatherImages[data.weather[0].main] : null
+
+    const backgroundImages = {
+        Clear: 'linear-gradient(to right, #f3b07c, #fcd283)',
+        Smoke: 'linear-gradient(to right, #d3d3d3, #949494)', 
+        Clouds: 'linear-gradient(to right, #57d6d4, #71eeec)',
+        Rain: 'linear-gradient(to right, #5bc8fb, #80eaff)',
+        Snow: 'linear-gradient(to right, #aff2f, #fff);',
+        Haze: 'linear-gradient(to right, #57d6d4, #71eeec)',
+        Mist: 'linear-gradient(to right, #57d6d4, #71eeec)',
+    }
+
+    const backgroundImage = data.weather ? backgroundImages[data.weather[0].main] : 'linear-gradient(to right, #f3b07c, #fcd283)'
+
+    const currentDate = new Date()
+
+    const daysOfWeek = [
+        'Sun', 
+        'Mon', 
+        'Tue', 
+        'Wed', 
+        'Thur', 
+        'Fri', 
+        'Sat']
+
+    const months = [
+        'Jan',
+        'Feb', 
+        'Mar', 
+        'Apr', 
+        'May', 
+        'Jun', 
+        'Jul', 
+        'Aug', 
+        'Sep', 
+        'Oct', 
+        'Nov', 
+        'Dec'
+    ]
+
+    const dayOfWeek = daysOfWeek[currentDate.getDay()]
+    const month = months[currentDate.getMonth()]
+    const dayOfMonth = currentDate.getDate()
+
+    const formattedDate = `${dayOfWeek}, ${dayOfMonth} ${month}`
+
   return (
-    <div className="container">
-        <div className="weather-app">
+    <div className="container" style={{backgroundImage}}>
+        <div className="weather-app" style={{backgroundImage : backgroundImage && backgroundImage.replace ? backgroundImage.replace("to right", "to top") : null}}>
             <div className="search">
                 <div className="search-top">
                     <i className="fa-solid fa-location-dot"></i>
@@ -47,25 +129,32 @@ const WeatherApp = () => {
                     <i className="fa-solid fa-magnifying-glass"onClick={search}></i>
                 </div>
             </div>
-            <div className="weather">
-                <img src={sunny} width={250} height={250} alt="sunny" />
-                <div className="weather-type">Clear</div>
-                <div className="temp">{data.main ? `(data.main.temp)°` : null}</div>
-            </div>
-            <div className="weather-date">
-                <p>Thur, 15 Aug</p>
-            </div>
-            <div className="weather-data">
-                <div className="humidity">
-                    <div className="data-name">Humidity</div>
-                    <i className="fa-solid fa-droplet"></i>
-                    <div className="data">35%</div>
-                </div>
-                <div className="wind">
-                    <div className="data-name">Wind</div>
-                    <i className="fa-solid fa-wind"></i>
-                    <div className="data">3 km/h</div></div>
-            </div>
+            {loading ? (<img className="loader" src={loadingGif}/>
+            ) : data.notFound ? (
+                <div className="not-found">Not Found 😭</div>) : (
+                <>
+                    <div className="weather">
+                        <img src={weatherImage} width={250} height={250} alt="sunny" />
+                        <div className="weather-type">{data.weather ? data.weather[0].main : null}</div>
+                        <div className="temp">{data.main ? `${Math.floor(data.main.temp)}°` : null}</div>
+                    </div>
+                    <div className="weather-date">
+                        <p>{formattedDate}</p>
+                    </div>
+                    <div className="weather-data">
+                        <div className="humidity">
+                            <div className="data-name">Humidity</div>
+                            <i className="fa-solid fa-droplet"></i>
+                            <div className="data">{data.main ? data.main.humidity : null}%</div>
+                        </div>
+                        <div className="wind">
+                            <div className="data-name">Wind</div>
+                            <i className="fa-solid fa-wind"></i>
+                            <div className="data">{data.wind ? data.wind.speed : null} mph</div></div>
+                    </div>
+            </>
+            )}
+            
         </div>
     </div>
   )
